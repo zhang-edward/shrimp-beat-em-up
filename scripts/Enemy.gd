0128e04532c541d14815c416afdfdb8ca28bc2d3
Edward Zhang <edwardz123@gmail.com>
2026-07-11 15:49:47 -0400

    Add state machine and basic enemy state

--- scripts/Enemy.gd
+++ scripts/Enemy.gd
@@ -1,8 +1,16 @@
 class_name Enemy
 extends CharacterBody2D
 
+@export var move_speed := 100.0
+
+@export var playerRef: Player
+var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
+
 func _ready() -> void:
 	pass
 
+func initialize(player: Player) -> void:
+	playerRef = player
+
 func _physics_process(delta: float) -> void:
-	pass
+	move_and_slide()
---
+++ scripts/states/AttackState.gd
@@ -0,0 +1,31 @@
+# Represents the state where the enemy is chasing and attacking the player. 
+class_name AttackState
+extends State
+
+const ATTACK_RANGE_X := 100.0
+const ATTACK_RANGE_Y := 20.0
+const ATTACK_COOLDOWN_SECONDS := 1.0
+
+var attack_timer := 0.0
+
+func update(_delta: float) -> void:
+	var enemy = entity as Enemy
+	var player = enemy.playerRef
+
+	enemy.velocity.x = sign(player.global_position.x - enemy.global_position.x) * enemy.move_speed \
+		if abs(player.global_position.x - enemy.global_position.x) > ATTACK_RANGE_X \
+		else 0.0
+
+	enemy.velocity.y = sign(player.global_position.y - enemy.global_position.y) * enemy.move_speed \
+		if abs(player.global_position.y - enemy.global_position.y) > ATTACK_RANGE_Y \
+		else 0.0
+
+	if abs(player.global_position.x - enemy.global_position.x) <= ATTACK_RANGE_X and \
+		abs(player.global_position.y - enemy.global_position.y) <= ATTACK_RANGE_Y:
+		enemy.velocity = Vector2.ZERO
+		# Attack player
+		if attack_timer <= 0.0:
+			attack_timer = ATTACK_COOLDOWN_SECONDS
+			print(enemy, " attacking player!")
+		else:
+			attack_timer -= _delta
